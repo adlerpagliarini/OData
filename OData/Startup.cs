0b625f7472fa91bdf675f8a227158d62d923073b
AdlerPagliarini <adlerpagliarini@gmail.com>
2019-04-14 19:31:28 -0300

    Removendo Swagger

--- OData/Startup.cs
+++ OData/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OData.Edm;
 using Microsoft.OData.UriParser;
 using OData.Data;
-using Swashbuckle.AspNetCore.Swagger;
 
 namespace OData
 {
@@ -26,19 +25,6 @@ namespace OData
             services.AddDbContext<DatabaseContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.AddSwaggerGen(s =>
-            {
-                s.SwaggerDoc("v1", new Info
-                {
-                    Version = "v1",
-                    Title = "OData API",
-                    Description = "API Open Data Protocol",
-                    TermsOfService = "Nenhum",
-                    Contact = new Contact { Name = "Adler Pagliarini", Email = "adlerpagliarini@gmail.com", Url = "localhost" },
-                    License = new License { Name = "MIT", Url = "localhost" }
-                });
-            });
 
             services.AddOData();
         }
@@ -68,12 +54,6 @@ namespace OData
                 
                 // Enables OData to all entities
                 //routerBuilder.Select().Filter().OrderBy().Count().Expand();
-            });
-
-            app.UseSwagger();
-            app.UseSwaggerUI(s =>
-            {
-                s.SwaggerEndpoint("/swagger/v1/swagger.json", "OData API v1.0");
             });
         }
     }
